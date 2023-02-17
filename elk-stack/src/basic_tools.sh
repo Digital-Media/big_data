374e48ae7a0461379f35df8a934fae617577da89
martinharrer <martin.harrer@fh-hagenberg.at>
2023-02-17 13:22:05 +0100

    cleaning up after finally switching to docker images and moving from filebeat to metricbeat examples

--- elk-stack/src/basic_tools.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "## Installing zip, unzip, curl, net-tools, ... ##"
-apt update
-apt install apt-utils software-properties-common
-apt-get -y install apt-transport-https curl git gnupg2 iputils-ping less nano net-tools telnet vim wget
